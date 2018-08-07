de790ef094e803cf771d1a85697ae0e621c54025
Zaiyiiiiiiiiii <wangzaiyiw@gmail.com>
2018-08-07 17:30:33 +0800

    1

--- src/components/buttons/LineHeightButton.vue
+++ src/components/buttons/LineHeightButton.vue
@@ -1,86 +1,33 @@
 <template>
     <hovershow-button icon="../static/lineheight.svg" :hideIcon="true">
-        <div class="lineheight-selector">
+        <div class="hovershow-selector">
             <span>行距</span>
             <input type="number" min="0" step="0.05" v-model="lineHeight" @change="changeLineHeight()">
         </div>
     </hovershow-button>
 </template>
 <style>
-    .lineheight-selector {
-        -webkit-app-region: no-drag;
-        height: 100%;
-        width: 140px;
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: flex-end;
-        overflow: visible;
-        padding-right: 20px;
-        box-sizing: border-box;
-        background: linear-gradient(
-            to right,
-            rgba(250, 250, 250, 0),
-            rgba(250, 250, 250, 0.6) 18%,
-            rgba(250, 250, 250, 0.9) 60%,
-            rgba(250, 250, 250, 1)
-        );
-        position: absolute;
-        top: 0;
-        right: 0;
-    }
-
-    .lineheight-selector>* {
-        color: #7E8282;
-        font-family: "default";
-        font-weight: lighter;
-    }
-
-    .lineheight-selector>input {
-        font-size: 1.15em;
-        height: 100%;
-        width: 1.85em;
-        outline: none;
-        border: none;
-        padding: 0;
-        margin: 0;
-        background: none;
-        text-align: right;
-        /* text-decoration-line: underline; */
-    }
-
-    .lineheight-selector>span {
-        font-size: 1.25em;
-        white-space: nowrap;
-        padding-right: 0.35em;
-        user-select: none;
-        cursor: default;
-    }
-
-    .lineheight-selector>input[type=number]::-webkit-inner-spin-button {
-        /* background: none;
-        opacity: 1; */
-        -webkit-appearance: none;
-    }
 </style>
 
 <script>
     import hoverShowButton from '../HoverShowButton.vue'
+    import hoverShow from '../../mixins/hovershow.vue'
     export default {
         data() {
             return {
                 lineHeight: 0
             }
         },
+        mixins:[hoverShow],
         components: {
             "hovershow-button": hoverShowButton
         },
         methods: {
             changeLineHeight() {
-                this.$store.commit("SET_STYLE",{styleName: "line-height", value: this.lineHeight})
+                this.$store.commit("SET_STYLE", { styleName: "line-height", value: this.lineHeight })
             },
-            async initLineHeight(){
-                return await this.$store.dispatch("getStyles", {styleName:"line-height", comp:this})
+            async initLineHeight() {
+                return await this.$store.dispatch("getStyles", { styleName: "line-height", comp: this })
             }
         },
         async mounted() {
---
+++ src/mixins/hovershow.vue
@@ -0,0 +1,61 @@
+<template>
+
+</template>
+
+<style>
+    .hovershow-selector {
+        -webkit-app-region: no-drag;
+        height: 100%;
+        width: 140px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: flex-end;
+        overflow: visible;
+        padding-right: 20px;
+        box-sizing: border-box;
+        background: linear-gradient(
+            to right,
+            rgba(250, 250, 250, 0),
+            rgba(250, 250, 250, 0.6) 18%,
+            rgba(250, 250, 250, 0.9) 60%,
+            rgba(250, 250, 250, 1)
+        );
+        position: absolute;
+        top: 0;
+        right: 0;
+    }
+
+    .hovershow-selector > * {
+        color: #7e8282;
+        font-family: "default";
+        font-weight: lighter;
+    }
+
+    .hovershow-selector > input {
+        font-size: 1.15em;
+        height: 100%;
+        width: 1.85em;
+        outline: none;
+        border: none;
+        padding: 0;
+        margin: 0;
+        background: none;
+        text-align: right;
+        /* text-decoration-line: underline; */
+    }
+
+    .hovershow-selector > span {
+        font-size: 1.25em;
+        white-space: nowrap;
+        padding-right: 0.35em;
+        user-select: none;
+        cursor: default;
+    }
+
+    .hovershow-selector > input[type="number"]::-webkit-inner-spin-button {
+        /* background: none;
+                opacity: 1; */
+        -webkit-appearance: none;
+    }
+</style>
